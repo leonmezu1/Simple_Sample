619cccc2168f8c933602afaafcd03e22d35c9d85
leonmezu1 <leo.mez@outlook.com>
2020-02-13 13:36:05 -0500

    Added email index, email pattern validaton and email uniqueness validation in the user model and database schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
+  validates :name, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :email, presence: true, length: { minimum: 10, maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
