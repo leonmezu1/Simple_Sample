e50f54b00133ed007f2d9a89a277bc17b63ad1fc
leonmezu1 <leo.mez@outlook.com>
2020-02-12 22:50:21 -0500

    Chapter 5 finished, title helper test was added and passed succesfully

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,5 +10,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', about_path, count: 0
     assert_select 'a[href=?]', contact_path, count: 0
+    get contact_path
+    assert_select 'title', full_title('Contact')
   end
 end
