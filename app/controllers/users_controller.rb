44db936aaa66b9729859eba79856b74f8d1b0e82
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-02-18 23:32:33 -0500

    Added the resources routes and  basic controller action for showing the user's information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,11 @@
 
 class UsersController < ApplicationController
   def new; end
+  def index; end
+  def create; end
+  def show;
+    @user = User.find(params[:id])
+  end
+  def update; end
+  def destroy; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :users, except: %i[new]
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
