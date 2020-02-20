afa095ab319931007d587a182193d3b137cbf158
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-02-19 22:08:00 -0500

    Refactores user controller adding the full RESTful methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,46 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def index; end
+  def index
+    @users = User.all
+  end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
 
   def show; end
 
-  def update; end
+  def edit; end
 
-  def destroy; end
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path
+  end
 
   private
 
   def set_params
     @user = User.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name,
+                                 :email, :password,
+                                 :password_confirmation)
   end
 end
