405cd3733b8305060f12905fe34e8ff18e6af07e
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-02-25 12:56:57 -0500

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      log_in user
-      remember user
-      redirect_to user_path(user)
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to user_path(@user)
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -2,7 +2,7 @@
 
 require 'test_helper'
 
-class UserLoginTest < ActionDispatch::IntegrationTest
+class UserLoginTest < ActionDispatch::IntegrationTest # rubocop: disable Metrics/ClassLength
   def setup
     @user = users(:michael)
   end
@@ -62,5 +62,19 @@ class UserLoginTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
     assert_select 'a[href=?]', user_path(@user), count: 0
+  end
+
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies[:remember_token]
+  end
+
+  test 'login without remembering' do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+    # Log in again and verify that the cookie is deleted.
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
   end
 end
