2c9501f5b20a1e5db2b4b80dde652cb9a4f01c7a
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-02-25 22:24:34 -0500

    Asserted div with messages for successful and unsuccesful login completed

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -10,12 +10,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test 'unsuccessful edit' do
     get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path(@user), params: { user: { name: '',
+    patch user_path(@user), params: { user: { name: '1234',
                                               email: 'foo@invalid',
                                               password: 'foo',
                                               password_confirmation: 'bar' } }
 
     assert_template 'users/edit'
-    assert_select 'div', text: 'alert'
+    assert_select 'div', attributes: { class: 'alert' }
+  end
+
+  test 'successful edit' do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    patch user_path(@user), params: { user:
+                                          { name: '1234asdf',
+                                            email: 'foo@invalid.com',
+                                            password: 'barvalid',
+                                            password_confirmation: 'barvalid' } }
+
+    assert_redirected_to user_path(@user)
+    assert_select 'div.alert', { count: 0 }
   end
 end
